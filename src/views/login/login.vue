0ff10072ce5ad09d8413654464a6ce718e044f93
zhb <1050299953@qq.com>
2021-01-11 09:06:24 +0800

    注册登陆提交

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,31 +1,111 @@
 <template>
-  <div id="login">
-    <Form :form="form">
-      <FormItem :labelCol="{ span: 6 }" :wrapperCol="{ span: 6 }" label="账号">
-        <Input
-          v-decorator="[
-            'name',
-            {
-              rules: [{ required: true, message: '请输入账号' }]
-            }
-          ]"
-        />
-      </FormItem>
-      <FormItem :labelCol="{ span: 6 }" :wrapperCol="{ span: 6 }" label="密码">
-        <Input
-          v-decorator="[
-            'password',
-            {
-              rules: [{ required: true, message: '请输入密码' }]
-            }
-          ]"
-        />
-      </FormItem>
-      <Button :style="{ marginLeft: '17vw' }" type="primary" @click="submit">
-        登陆
-      </Button>
-      <Button :style="{ marginLeft: '5vw' }">注册</Button>
-    </Form>
+  <div>
+    <div id="login" v-show="registerShow === false">
+      <Form :form="form">
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="用户名"
+        >
+          <Input
+            v-decorator="[
+              'name',
+              {
+                rules: [{ required: true, message: '请输入用户名' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="密码"
+        >
+          <Input
+            v-decorator="[
+              'password',
+              {
+                rules: [{ required: true, message: '请输入密码' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <Button :style="{ marginLeft: '17vw' }" type="primary" @click="submit">
+          登陆
+        </Button>
+        <Button @click="register" :style="{ marginLeft: '5vw' }">注册</Button>
+      </Form>
+    </div>
+    <div id="login" v-show="registerShow === true">
+      <Form :form="registerForm">
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="用户名"
+        >
+          <Input
+            v-decorator="[
+              'name',
+              {
+                rules: [{ required: true, message: '请输入用户名' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="密码"
+        >
+          <Input
+            v-decorator="[
+              'password',
+              {
+                rules: [{ required: true, message: '请输入密码' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="确认密码"
+        >
+          <Input
+            v-decorator="[
+              'newpassword',
+              {
+                rules: [{ required: true, message: '请重复输入密码' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <FormItem
+          :labelCol="{ span: 6 }"
+          :wrapperCol="{ span: 6 }"
+          label="验证码"
+        >
+          <Input
+            v-decorator="[
+              'code',
+              {
+                rules: [{ required: true, message: '请输入验证码' }]
+              }
+            ]"
+          />
+        </FormItem>
+        <Button
+          :style="{ marginLeft: '17vw' }"
+          type="primary"
+          @click="register"
+        >
+          去登陆
+        </Button>
+        <Button @click="registerSub" :style="{ marginLeft: '5vw' }"
+          >注册</Button
+        >
+      </Form>
+    </div>
   </div>
 </template>
 
@@ -36,9 +116,14 @@ export default {
   components: { Form, FormItem, Input, Button },
   beforeCreate() {
     this.form = this.$form.createForm(this, { name: "normal_login" });
+    this.registerForm = this.$form.createForm(this, {
+      name: "normal_register"
+    });
   },
   data() {
-    return {};
+    return {
+      registerShow: false
+    };
   },
   methods: {
     submit() {
@@ -47,6 +132,16 @@ export default {
           this.$api.signUp(values).then(res => {
             console.log(res);
           });
+        }
+      });
+    },
+    register() {
+      this.registerShow = !this.registerShow;
+    },
+    registerSub() {
+      this.registerForm.validateFields((errors, values) => {
+        if (!errors) {
+          console.log(values);
         }
       });
     }
