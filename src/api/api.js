743211efe04ca96560546ba93bc7a1435e2e1d16
zhb <1050299953@qq.com>
2021-01-14 17:19:06 +0800

    代码提交

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 import request from "./request";
 
-const BaseUrl = "http://193.112.205.103:8819";
+const BaseUrl = "http://193.112.205.103:8819/api/v1";
 
 const signUp = () =>
   request({
-    url: BaseUrl + "/user/signup",
-    type: "post"
+    url: BaseUrl + "/register/login",
+    method: "POST"
   });
 
 export default {
